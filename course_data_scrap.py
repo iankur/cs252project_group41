dcb504a48cbb14b43c1327028a53748c6fa8df2a
Kritagya <kdabi@iitk.ac.in>
2016-10-31 14:35:14 +0530

    putting data in csv

--- course_data_scrap.py
+++ course_data_scrap.py
@@ -1,4 +1,5 @@
 import httplib2
+import csv
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
@@ -14,11 +15,11 @@ for a_Tag in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')).find_all(
 
 courses = []
 
-for course_link in courses_links[:]:
+for course_link in courses_links[:10]:
 	r = requests.get(course_link)
 	soup = BeautifulSoup(r.text)
 	
-	course_details = {}
+	course_details = []
 	for course_data in soup.select('.page'):
 		h1 = len(course_data.findAll('h1'))
 		h2 = len(course_data.findAll('h2'))
@@ -28,35 +29,45 @@ for course_link in courses_links[:]:
                 h6 = len(course_data.findAll('h6'));
                 p = len(course_data.findAll('p'));
 		ol = len(course_data.findAll('ol'));
-		course_details['Credits'] = ''
-		course_details['Pre-req'] = ''
-                course_details['Content'] = ''
-		if h1 > 0 :	
-			course_details['Name'] = course_data.findAll('h1')[0].text.strip()
-		elif h2 > 0:
-			course_details['Name'] = course_data.findAll('h2')[0].text.strip()
-		elif p > 0:
-			course_details['Name'] = course_data.findAll('p')[0].text.strip()
+		k = ''
+                course_details.append(k)
+		course_details.append(k)
+		course_details.append(k)
+                course_details.append(k)
+		if ol > 0 :
+			course_content = []
+                        li = len(course_data.findAll('ol')[0].findAll('li'))
+                        for k in range(0,li):
+				course_details[3] = course_details[3] + "\n" + course_data.findAll('li')[k].text.strip()
 		
 		for k in range(0,h5) :
 			if 'Units:' in course_data.findAll('h5')[k].text.strip() :
-				course_details['Credits'] = course_data.findAll('h5')[k].text.strip()
+				course_details[2] = course_data.findAll('h5')[k].text.strip()
 		for k in range(0,p) :
 			if 'Units:' in course_data.findAll('p')[k].text.strip() :
-				course_details['Credits'] = course_data.findAll('p')[k].text.strip()
+				course_details[2] = course_data.findAll('p')[k].text.strip()
 		for k in range(0,h5) :
 			if 'Pre-re' in course_data.findAll('h5')[k].text.strip() :
-				course_details['Pre-req'] = course_data.findAll('h5')[k].text.strip()
+				course_details[1] = course_data.findAll('h5')[k].text.strip()
 		for k in range(0,p) :
 			if 'Pre-re' in course_data.findAll('p')[k].text.strip() :
-				course_details['Pre-req'] = course_data.findAll('p')[k].text.strip()
-                
-                if ol > 0 :
-			course_details['Content'] = course_data.findAll('ol')[0].text.strip()
-		print  'Name = '  + course_details['Name']
-		print  'Credit = ' + course_details['Credits']
-		print 'Pre-requisites = ' + course_details['Pre-req']
-		print 'Content = ' + course_details['Content']
+				course_details[1] = course_data.findAll('p')[k].text.strip()  
+		if h1 > 0 :	
+			course_details[0] = course_data.findAll('h1')[0].text.strip()
+		elif h2 > 0:
+			course_details[0] = course_data.findAll('h2')[0].text.strip()
+		elif p > 0:
+			course_details[0] = course_data.findAll('p')[0].text.strip()
+		
+		              			
+		print  course_details[0]
+		print course_details[2]
+		print course_details[1]
+		print 'Content : ' + course_details[3][:]
 	
 	courses.append(course_details)	
-
+with open ('data.csv','wb') as file:
+   writer=csv.writer(file)
+   writer = csv.writer(file, delimiter = '$')
+   for row in courses:
+      writer.writerow([row])
